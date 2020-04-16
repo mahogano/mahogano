25fb5a9022926c9e6202f6abf68db65731f4518d
Rostyslav Rava <rostyslav.rava@gmail.com>
2020-04-16 18:52:09 +0000

    Another one test for Travis build

--- src/main/java/com/mahogano/core/magento/entity/CustomerAddress.java
+++ src/main/java/com/mahogano/core/magento/entity/CustomerAddress.java
@@ -1,4 +1,101 @@
 package com.mahogano.core.magento.entity;
 
+import java.time.LocalDateTime;
+
 public class CustomerAddress {
+
+    private Integer entityId;
+
+    private Integer entityTypeId;
+
+    private Integer attributeSetId;
+
+    private String incrementId;
+
+    private Integer parentId;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Integer isActive;
+
+    public Integer getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(Integer entityId) {
+        this.entityId = entityId;
+    }
+
+    public Integer getEntityTypeId() {
+        return entityTypeId;
+    }
+
+    public void setEntityTypeId(Integer entityTypeId) {
+        this.entityTypeId = entityTypeId;
+    }
+
+    public Integer getAttributeSetId() {
+        return attributeSetId;
+    }
+
+    public void setAttributeSetId(Integer attributeSetId) {
+        this.attributeSetId = attributeSetId;
+    }
+
+    public String getIncrementId() {
+        return incrementId;
+    }
+
+    public void setIncrementId(String incrementId) {
+        this.incrementId = incrementId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Integer isActive) {
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerAddress{" +
+                "entityId=" + entityId +
+                ", entityTypeId=" + entityTypeId +
+                ", attributeSetId=" + attributeSetId +
+                ", incrementId='" + incrementId + '\'' +
+                ", parentId=" + parentId +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", isActive=" + isActive +
+                '}';
+    }
+
 }
