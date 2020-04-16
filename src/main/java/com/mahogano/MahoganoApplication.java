f979d2e4365d47415118063795719ad519f3c8d4
Rostyslav Rava <rostyslav.rava@gmail.com>
2020-04-16 13:10:50 +0000

    Added basic project structure

---
+++ src/main/java/com/mahogano/MahoganoApplication.java
@@ -0,0 +1,13 @@
+package com.mahogano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahoganoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahoganoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahogano/MahoganoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahogano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahoganoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
